de75a33c4133c3a72093893d34d7e51215dfa1cf
Martin Hansson <minihansson@hotmail.com>
2013-12-22 15:38:17 +0100

    Widget forts
    Enkel widget för att se hur det fungerar

--- P9-Custom-Post.php
+++ P9-Custom-Post.php
@@ -52,10 +52,64 @@ function register_cpt_p9_custom_post() {
 
   register_post_type( 'p9-custom-post', $args );
 }
-// Load widget
-function p9_custom_post_widget_load_widgets() {
-	require( 'P9-Custom-Post-Widget.php' );
-	register_widget( 'P9_custom_post_widget' );
+// Creating the widget 
+class p9_custom_post_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+// Base ID of your widget
+'p9_custom_post_widget', 
+
+// Widget name will appear in UI
+__('P9 Custom Posts', 'p9_custom_post_widget_domain'), 
+
+// Widget description
+array( 'description' => __( 'Produktion9 Custom Post Type', 'p9_custom_post_widget_domain' ), ) 
+);
 }
-add_action( 'widgets_init', 'P9_custom_post_widget' );
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+echo __( 'Hello, World!', 'p9_custom_post_widget_domain' );
+echo $args['after_widget'];
+}
+		
+// Widget Backend 
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'p9_custom_post_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+	
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class p9_custom_post_widget ends here
+
+// Register and load the widget
+function p9_custom_post_load_widget() {
+	register_widget( 'p9_custom_post_widget' );
+}
+add_action( 'widgets_init', 'p9_custom_post_load_widget' );
 ?>
